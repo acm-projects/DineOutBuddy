eed914d063878eb136fc079d19c38c758e36205e
WyShadow11 <luujason11@gmail.com>
2024-02-15 13:13:25 -0600

    Implemented Hasing Password to improve security

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from"mongoose";
+import bcrypt from 'bcrypt';
 
 const userSchema = mongoose.Schema(
   {
@@ -35,6 +36,27 @@ const userSchema = mongoose.Schema(
     },
   }
 )
+
+userSchema.pre('save', function(next){
+  if(this.isModified('password')){
+    bcrypt.hash(this.password, 8, (err, hash) => {
+      if (err) return next (err);
+
+      this.password = hash;
+      next();
+    })
+  }
+})
+
+userSchema.methods.comparePassword = async function(password){
+  if(!password) throw new Error("Password is missing,can not compare!");
+  try {
+    const result = await bcrypt.compare(password, this.password);
+    return result;
+  } catch (error) {
+    console.log("Error while comparing password!", error.message);
+  }
+}
 
 userSchema.statics.isThisEmailInUse = async function(email){
   if (!email) throw new Error("Invalid Email")
