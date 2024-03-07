9c5b98bed9ffbbf118ea4c868d0a7d02d92b9288
WyShadow11 <luujason11@gmail.com>
2024-03-07 13:09:47 -0600

    Fixed App.js so it compiles

--- frontend/app.js
+++ frontend/app.js
@@ -1,22 +1,80 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './components/Home/Home';
-import Login from './components/Login/Login';
-import Questionnare from './components/Questionnare/Questionnare'; // Make sure this is correctly spelled
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import FormHeader from "./app/components/FormHeader";
+import FormSelectorBtn from "./app/components/FormSelectorBtn";
+import LoginForm from "./app/components/LoginForm";
+import axios from "axios";
+import SignupForm from "./app/components/SignupForm";
+import { useEffect } from "react";
 
-const Stack = createNativeStackNavigator();
+export default function App() {
+  const fetchApi = async () => {
+    try {
+      const res = await axios.get("http://127.0.0.1:8000/");
+      console.log(res);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
-const App = () => {
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Questionnare" component={Questionnare} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+    <View style={{ flex: 2, paddingTop: 60 }}>
+      <View style={{ height: 80 }}>
+        <FormHeader
+          leftHeading="Welcome"
+          rightHeading="Back"
+          subHeading="DineOutBuddy"
+        />
+      </View>
 
-export default App;
+      <View
+        style={{
+          flexDirection: "row",
+          paddingHorizontal: 20,
+          marginBottom: 20,
+        }}
+      >
+        <FormSelectorBtn
+          backgroundColor="#0093ED"
+          title="Log In"
+          style={styles.borderLeft}
+        />
+        <FormSelectorBtn
+          backgroundColor="rgba(27,27,51,0.4)"
+          title="Sign Up"
+          style={styles.borderRight}
+        />
+      </View>
+
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        style={{ backgroundColor: "white" }}
+      >
+        <LoginForm />
+        <SignupForm />
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  borderLeft: {
+    borderTopLeftRadius: 8,
+    borderBottomLeftRadius: 8,
+  },
+  borderRight: {
+    borderTopRightRadius: 8,
+    borderBottomRightRadius: 8,
+  },
+});
