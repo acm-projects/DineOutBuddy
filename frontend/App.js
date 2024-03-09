d72cbcc305ff41a67ff9b3cece9153780add20cd
WyShadow <luujason11@gmail.com>
2024-03-09 09:56:08 -0600

    Refactored code to include Stack Navigation

--- frontend/App.js
+++ frontend/App.js
@@ -1,80 +1,19 @@
-import { StatusBar } from "expo-status-bar";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
-import FormHeader from "./app/components/FormHeader";
-import FormSelectorBtn from "./app/components/FormSelectorBtn";
-import LoginForm from "./app/components/LoginForm";
-import axios from "axios";
-import SignupForm from "./app/components/SignupForm";
-import { useEffect } from "react";
+import React from "react";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import AppForm from "./app/screens/AppForm";
+import Test from "./app/screens/Test";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const fetchApi = async () => {
-    try {
-      const res = await axios.get("http://127.0.0.1:8000/");
-      console.log(res);
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
-  useEffect(() => {
-    fetchApi();
-  }, []);
-
   return (
-    <View style={{ flex: 2, paddingTop: 60 }}>
-      <View style={{ height: 80 }}>
-        <FormHeader
-          leftHeading="Welcome"
-          rightHeading="Back"
-          subHeading="DineOutBuddy"
-        />
-      </View>
-
-      <View
-        style={{
-          flexDirection: "row",
-          paddingHorizontal: 20,
-          marginBottom: 20,
-        }}
-      >
-        <FormSelectorBtn
-          backgroundColor="#0093ED"
-          title="Log In"
-          style={styles.borderLeft}
-        />
-        <FormSelectorBtn
-          backgroundColor="rgba(27,27,51,0.4)"
-          title="Sign Up"
-          style={styles.borderRight}
-        />
-      </View>
-
-      <ScrollView
-        horizontal
-        pagingEnabled
-        showsHorizontalScrollIndicator={false}
-        style={{ backgroundColor: "white" }}
-      >
-        <LoginForm />
-        <SignupForm />
-      </ScrollView>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen component={AppForm} name="AppForm" />
+        <Stack.Screen component={Test} name="test" />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  borderLeft: {
-    borderTopLeftRadius: 8,
-    borderBottomLeftRadius: 8,
-  },
-  borderRight: {
-    borderTopRightRadius: 8,
-    borderBottomRightRadius: 8,
-  },
-});
---
+++ frontend/app/screens/AppForm.js
@@ -0,0 +1,80 @@
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import FormHeader from "../components/FormHeader";
+import FormSelectorBtn from "../components/FormSelectorBtn";
+import LoginForm from "../components/LoginForm";
+import axios from "axios";
+import SignupForm from "../components/SignupForm";
+import { useEffect } from "react";
+
+export default function AppForm() {
+  const fetchApi = async () => {
+    try {
+      const res = await axios.get("http://127.0.0.1:8000/");
+      console.log(res);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
+  return (
+    <View style={{ flex: 2, paddingTop: 60 }}>
+      <View style={{ height: 80 }}>
+        <FormHeader
+          leftHeading="Welcome"
+          rightHeading="Back"
+          subHeading="DineOutBuddy"
+        />
+      </View>
+
+      <View
+        style={{
+          flexDirection: "row",
+          paddingHorizontal: 20,
+          marginBottom: 20,
+        }}
+      >
+        <FormSelectorBtn
+          backgroundColor="#0093ED"
+          title="Log In"
+          style={styles.borderLeft}
+        />
+        <FormSelectorBtn
+          backgroundColor="rgba(27,27,51,0.4)"
+          title="Sign Up"
+          style={styles.borderRight}
+        />
+      </View>
+
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        style={{ backgroundColor: "white" }}
+      >
+        <LoginForm />
+        <SignupForm />
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  borderLeft: {
+    borderTopLeftRadius: 8,
+    borderBottomLeftRadius: 8,
+  },
+  borderRight: {
+    borderTopRightRadius: 8,
+    borderBottomRightRadius: 8,
+  },
+});
