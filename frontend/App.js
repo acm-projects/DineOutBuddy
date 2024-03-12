985f2a263298a27990bd43903bf8b6768350d212
WyShadow <luujason11@gmail.com>
2024-03-12 12:18:55 -0500

    Created temp frontend for groupFeature

--- frontend/App.js
+++ frontend/App.js
@@ -4,6 +4,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import AppForm from "./app/screens/AppForm";
 import Test from "./app/screens/Test";
+import MessagesScreen from "./app/screens/MessageScreen";
+import ChatScreen from "./app/screens/ChatScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +15,8 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen component={AppForm} name="AppForm" />
         <Stack.Screen component={Test} name="test" />
+        <Stack.Screen component={MessagesScreen} name="MessagesScreen" />
+        <Stack.Screen component={ChatScreen} name="ChatScreen" />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- frontend/app/screens/Test.js
+++ frontend/app/screens/Test.js
@@ -1,10 +1,14 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 import React from "react";
 
-const Test = () => {
+const Test = ({ navigation }) => {
   return (
     <View>
       <Text>Test</Text>
+      <Button
+        title="Go to GroupPage"
+        onPress={() => navigation.navigate("MessagesScreen")}
+      />
     </View>
   );
 };
