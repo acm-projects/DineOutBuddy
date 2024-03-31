5a4a87608b5cb6efb2d1c665ce69b9cbc45a8aa2
toastybridge <jtan208295@gmail.com>
2024-03-31 17:14:10 -0500

    Added pages for preference selection

---
+++ frontend/app/tabs/GetAllergiesScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, TouchableOpacity } from "react-native";
+import { accentColor } from "../components/ComponentColors";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+export default function AllergiesStack ()
+{
+		<Stack.Navigator>
+			<Stack.Screen component={AllergyScreen} name="AllergyScreen" />
+			<Stack.Screen component={PreferencesScreen} name="PreferencesScreen" />
+			<Stack.Screen component={CravingsScreen} name="CravingsScreen" />
+		</Stack.Navigator>
+}
