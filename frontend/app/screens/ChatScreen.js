e01dd48f21514af027d81a958b02b37b0df50302
Jason Luu <luujason11@gmail.com>
2024-04-11 00:40:42 -0500

    Update ChatScreen.js

--- frontend/app/screens/ChatScreen.js
+++ frontend/app/screens/ChatScreen.js
@@ -59,7 +59,7 @@ export default function Messagescreen({ route }) {
   }, [chat]);
 
   useEffect(() => {
-    socket = io("http://192.168.50.72:8000");
+    socket = io("http://IPADDRSES:8000");
     socket.emit("setup", profile);
     socket.on("connected", () => setSocketConnected(true));
     socket.on("typing", () => setIsTyping(true));
