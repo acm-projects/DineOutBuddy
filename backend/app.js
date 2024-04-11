d15b19723c53c4ddc7fb41c5a635779d8b721f69
Jason Luu <luujason11@gmail.com>
2024-04-11 00:41:31 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ mongoose
     const io = new Server(httpServer, {
       pingTimeout: 60000,
       cors: {
-        origin: "http://192.168.50.72:8081",
+        origin: "http://IPADDRSES:8081",
       },
     });
 
